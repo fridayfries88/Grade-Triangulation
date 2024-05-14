09a8c2d9c5caae6407b02da3e4685152b7cbc18a
Alex-Pychyl <alex.pychyl@gmail.com>
2024-05-14 12:50:00 -0400

    Moved files around

---
+++ GradeTriangulationWebApp/src/main/java/alexshruthika/webapp/servlets/NewClass.java
@@ -0,0 +1,80 @@
+package alexshruthika.webapp.servlets;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+
+/**
+ *
+ * @author alexp
+ */
+public class NewClass extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String courseCode = request.getParameter("courseCode");
+        String numStudents = request.getParameter("numStudents");
+        if (courseCode != null && Integer.parseInt(numStudents) > 0) {
+            request.setAttribute("courseCode", "");
+            response.getWriter().println("<html>\n<p>" + courseCode + " " + numStudents + "</p>\n</html>");
+        }
+        request.getRequestDispatcher("/WEB-INF/new-class.jsp").include(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Web page for creating a new class of students";
+    }// </editor-fold>
+
+}
