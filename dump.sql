eaa58702bccc6139fa8094884f980d9661787a54
Alex-Pychyl <alex.pychyl@gmail.com>
2024-06-12 10:11:18 -0400

    erased all users

--- dump.sql
+++ dump.sql
@@ -16,90 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `assignment21`
---
-
-DROP TABLE IF EXISTS `assignment21`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `assignment21` (
-  `assignment21_student_id` int unsigned NOT NULL,
-  `text_Text` varchar(500) DEFAULT NULL,
-  `grade_Grade` varchar(500) DEFAULT NULL,
-  `check_Yes/No` varchar(500) DEFAULT NULL,
-  `percent_Percentage` varchar(500) DEFAULT NULL,
-  `Additional notes_Text` varchar(500) DEFAULT NULL,
-  KEY `assignment21_student_id` (`assignment21_student_id`),
-  CONSTRAINT `assignment21_student_id` FOREIGN KEY (`assignment21_student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `assignment21`
---
-
-LOCK TABLES `assignment21` WRITE;
-/*!40000 ALTER TABLE `assignment21` DISABLE KEYS */;
-INSERT INTO `assignment21` VALUES (22,'n','1-','yes','42','Good'),(23,'','','','',''),(24,'','','','',''),(25,'','','','',''),(26,'','3-','','',''),(27,'','','','',''),(28,'','','','',''),(29,'gyf','','yes','',''),(30,'','','','',''),(31,'','','','',''),(32,'','','','',''),(33,'','4+','','',''),(34,'','','','',''),(35,'','','','',''),(36,'','','','',''),(37,'','','','',''),(39,'','','','','');
-/*!40000 ALTER TABLE `assignment21` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `assignment22`
---
-
-DROP TABLE IF EXISTS `assignment22`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `assignment22` (
-  `assignment22_student_id` int unsigned NOT NULL,
-  `grade_Grade` varchar(500) DEFAULT NULL,
-  `grade 2_Grade` varchar(500) DEFAULT NULL,
-  `text box_Text` varchar(500) DEFAULT NULL,
-  `Additional notes_Text` varchar(500) DEFAULT NULL,
-  KEY `assignment22_student_id` (`assignment22_student_id`),
-  CONSTRAINT `assignment22_student_id` FOREIGN KEY (`assignment22_student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `assignment22`
---
-
-LOCK TABLES `assignment22` WRITE;
-/*!40000 ALTER TABLE `assignment22` DISABLE KEYS */;
-INSERT INTO `assignment22` VALUES (22,'2-','4-','sdf','Good'),(23,'','','',''),(24,'','','g',''),(25,'','','',''),(26,'','','',''),(27,'','','',''),(28,'','','',''),(29,'','','',''),(30,'','','',''),(31,'','','',''),(32,'','','',''),(33,'','','',''),(34,'','','',''),(35,'','','',''),(36,'','','',''),(37,'','','',''),(39,NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `assignment22` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `assignment24`
---
-
-DROP TABLE IF EXISTS `assignment24`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `assignment24` (
-  `assignment24_student_id` int unsigned NOT NULL,
-  `used volumetric flask_Checkbox` varchar(500) DEFAULT NULL,
-  `layed pipette perpendicular_Checkbox` varchar(500) DEFAULT NULL,
-  `Additional notes_Text` varchar(500) DEFAULT NULL,
-  KEY `assignment24_student_id` (`assignment24_student_id`),
-  CONSTRAINT `assignment24_student_id` FOREIGN KEY (`assignment24_student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `assignment24`
---
-
-LOCK TABLES `assignment24` WRITE;
-/*!40000 ALTER TABLE `assignment24` DISABLE KEYS */;
-INSERT INTO `assignment24` VALUES (22,'x','x','1'),(23,'','','2'),(24,'x','','3'),(25,'','','4'),(26,'','',''),(27,'','',''),(28,'','',''),(29,'','',''),(30,'','',''),(31,'','',''),(32,'','',''),(33,'','',''),(34,'','',''),(35,'','',''),(36,'','',''),(37,'','',''),(39,'','','');
-/*!40000 ALTER TABLE `assignment24` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `assignments`
 --
 
@@ -115,7 +31,7 @@ CREATE TABLE `assignments` (
   PRIMARY KEY (`id`),
   KEY `class_id` (`assignment_class_id`),
   CONSTRAINT `class_id` FOREIGN KEY (`assignment_class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,7 +40,6 @@ CREATE TABLE `assignments` (
 
 LOCK TABLES `assignments` WRITE;
 /*!40000 ALTER TABLE `assignments` DISABLE KEYS */;
-INSERT INTO `assignments` VALUES (21,10,'Lab','Lab 1',NULL),(22,10,'Lab','Lab 2',NULL),(24,10,'Lab','Lab 3',NULL);
 /*!40000 ALTER TABLE `assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +60,7 @@ CREATE TABLE `classes` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +69,6 @@ CREATE TABLE `classes` (
 
 LOCK TABLES `classes` WRITE;
 /*!40000 ALTER TABLE `classes` DISABLE KEYS */;
-INSERT INTO `classes` VALUES (10,7,'ICS4U.01',2024,2,4);
 /*!40000 ALTER TABLE `classes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,7 +87,7 @@ CREATE TABLE `students` (
   PRIMARY KEY (`id`),
   KEY `student_class_id` (`student_class_id`),
   CONSTRAINT `student_class_id` FOREIGN KEY (`student_class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,7 +96,6 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (22,10,'a','aa'),(23,10,'b','bb'),(24,10,'c','cc'),(25,10,'d','dd'),(26,10,'e','ee'),(27,10,'f','ff'),(28,10,'g','gg'),(29,10,'h','hh'),(30,10,'i','ii'),(31,10,'j','jj'),(32,10,'k','kk'),(33,10,'l','ll'),(34,10,'m','mm'),(35,10,'n','nn'),(36,10,'o','oo'),(37,10,'p','pp'),(39,10,'q','qq');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,7 +146,7 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -242,7 +155,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (7,'Alex Pychyl','password'),(8,'Alex','password');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -255,4 +167,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-10 20:59:01
+-- Dump completed on 2024-06-12 10:10:57
